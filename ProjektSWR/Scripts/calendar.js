348f2e46828175fa81ce9b93c623a3b2c25cc9a2
misiek4895 <michaaal.b@gmail.com>
2017-03-19 14:29:30 +0100

    Revert "Kalendarz"
    This reverts commit 2c188f667e60e1de4b29fb9a4fc0702e4e461570.

--- ProjektSWR/Scripts/calendar.js
+++ ProjektSWR/Scripts/calendar.js
@@ -1,64 +1,86 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
     $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultDate: '2017-02-12',
+        navLinks: true, // can click day/week names to navigate views
+        selectable: true,
+        selectHelper: true,
+        select: function (start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2017-02-01'
             },
-            defaultDate: '2017-03-20',
-            locale: 'pl',
-            navLinks: true, // can click day/week names to navigate views
-            selectable: true,
-            selectHelper: true,
-            select: function (start, end) {
-                var title = prompt('Tytuł nowego zdarzenia:');
-                var eventData;
-                if (title) {
-                    eventData = {
-                        title: title,
-                        start: start,
-                        end: end
-                    };
-                    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-                }
-                $('#calendar').fullCalendar('unselect');
+            {
+                title: 'Long Event',
+                start: '2017-02-07',
+                end: '2017-02-10'
             },
-            editable: true,
-            eventLimit: true, // allow "more" link when too many events
-            events: [
-                {
-                    title: 'Bazy danych \n (F2) - W30 \n Wykład \n Trybus Bartosz (EA)',
-                    start: '2017-03-21T08:00:00',
-                    end: '2017-03-21T10:00:00'
-                },
-                {
-                    title: 'Sieci Komputerowe I \n (F2) - W30 \n Wykład \n Bolanowski Marek (EE)',
-                    start: '2017-03-21T10:00:00',
-                    end: '2017-03-21T12:00:00'
-                },
-                {
-                    title: 'Podstawy telekomunikacji \n (F2) - W15 \n Wykład \n Gotfryd Marek (EU) \n Pawlowicz Bartosz (EU)',
-                    start: '2017-03-21T12:00:00',
-                    end: '2017-03-21T14:00:00'
-                },
-                {
-                    title: 'Sztuczna inteligencja \n (F2) - W30 \n Wykład \n Kluska Jacek (EA)',
-                    start: '2017-03-21T14:00:00',
-                    end: '2017-03-21T16:00:00'
-                },
-                {
-                    title: 'Grafika komputerowa \n (F2) - W30 \n Wykład \n Leniowski Ryszard (EA)',
-                    start: '2017-03-23T08:00:00',
-                    end: '2017-03-23T10:00:00'
-                },
-                {
-                    title: 'Inżynieria oprogramowania \n (F2) - W30 \n Wykład \n Dec Grzegorz (EA)',
-                    start: '2017-03-23T10:00:00',
-                    end: '2017-03-23T12:00:00'
-                }
-            ]
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2017-02-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2017-02-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2017-02-11',
+                end: '2017-02-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2017-02-12T10:30:00',
+                end: '2017-02-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2017-02-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2017-02-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2017-02-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2017-02-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2017-02-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2017-02-28'
+            }
+        ]
     });
 
 });
